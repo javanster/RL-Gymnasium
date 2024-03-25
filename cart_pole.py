3ad9f3f834650e04ef7ab7b27814264354005d67
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-03-25 17:53:27 +0100

    Add documentation

--- cart_pole.py
+++ cart_pole.py
@@ -5,6 +5,19 @@ import sys
 
 
 class CartPoleQLearningAgent:
+    """
+    A class for an agent to solve the Cart Pole problem using Q learning
+    with epsilon-greedy strategy
+
+    Inspired by an implementation by Aleksandar Haber: https://aleksandarhaber.com/q-learning-in-python-with-tests-in-cart-pole-openai-gym-environment-reinforcement-learning-tutorial/
+
+    :param alpha: The learning rate, typically 0.1, 0.01 or 0.001
+    :param gamma: The discount rate between 0 (myopic) and 1 (future rewards more important)
+    :param epsilon: The exploration / exploitation trade-off. Between 0 (no exploration) and 1 (no exploitation)
+    :param bin_counts: The number of bins each continuous state space should be divided into
+    :param episode_count: The number of episodes to use for training
+    :param time_steps: The max number of time steps to run the testing for
+    """
 
     def __init__(self, alpha, gamma, epsilon, bin_counts, episode_count, time_steps) -> None:
         self.alpha = alpha
@@ -13,12 +26,13 @@ class CartPoleQLearningAgent:
         self.train_env = gym.make("CartPole-v1")
         self.test_env = gym.make("CartPole-v1", render_mode="human")
 
+        # Setting bounds for bins
         upper_bounds = self.train_env.observation_space.high
         lower_bounds = self.train_env.observation_space.low
-        cartVelocityMin = -3
-        cartVelocityMax = 3
-        poleAngleVelocityMin = -10
-        poleAngleVelocityMax = 10
+        cartVelocityMin = -3    # Based on observations
+        cartVelocityMax = 3     # Based on observations
+        poleAngleVelocityMin = -10  # Based on observations
+        poleAngleVelocityMax = 10   # Based on observations
         upper_bounds[1] = cartVelocityMax
         upper_bounds[3] = poleAngleVelocityMax
         lower_bounds[1] = cartVelocityMin
@@ -34,6 +48,15 @@ class CartPoleQLearningAgent:
         self.time_steps = time_steps
 
     def get_bins(self, lower_bounds, upper_bounds, bin_counts):
+        """
+        Get bins used for transforming the contiuous state space to a discrete one.
+        Necessary as the Q-matrix does not allow for a continuous state space.
+
+        :param lower_bounds: The min value each of the four state parameters may have
+        :param upper_bounds: The max value each of the four state parameters may have
+        :param bin_counts: The number of bins each state space of each state parameter
+        should be divided into
+        """
         cart_position_bins = np.linspace(
             lower_bounds[0], upper_bounds[0], bin_counts[0])
         cart_velocity_bins = np.linspace(
@@ -45,35 +68,12 @@ class CartPoleQLearningAgent:
 
         return [cart_position_bins, cart_velocity_bins, pole_angle_bins, pole_velocity_bins]
 
-    # Test simulation of the environment with random steps, finding typical velocity range
-    def find_typical_velocity_range(self):
-        cart_velocity_max = sys.float_info.min
-        cart_velocity_min = sys.float_info.max
-        pole_velocity_max = sys.float_info.min
-        pole_velocity_min = sys.float_info.max
-
-        for _ in range(self.episode_count):
-            self.train_env.reset()
-            self.train_env.render()
-            for _ in range(self.time_steps):
-                random_action = self.train_env.action_space.sample()
-                observation, _, terminated, _, _ = self.train_env.step(
-                    random_action)
-                cart_velocity_max = max(cart_velocity_max, observation[1])
-                cart_velocity_min = min(cart_velocity_min, observation[1])
-                pole_velocity_max = max(pole_velocity_max, observation[3])
-                pole_velocity_min = min(pole_velocity_min, observation[3])
-
-                if terminated:
-                    break
-
-        self.train_env.close()
-        return tuple([cart_velocity_max, cart_velocity_min, pole_velocity_max, pole_velocity_min])
-
     def get_state_index(self, state):
         """
         Returns a tuple of the index of the bin (discrete) which matches the state,
         since the state space is continuous (incompatible with the Q-matrix)
+
+        :param state: The current state
         """
         cart_position = state[0]
         cart_velocity = state[1]
@@ -91,13 +91,25 @@ class CartPoleQLearningAgent:
 
         return tuple([cart_position_index, cart_velocity_index, pole_angle_index, pole_velocity_index])
 
-    def select_action(self, state, episode_index):
-        if episode_index < 500:
+    def select_action(self, state, episode):
+        """
+        Select an action for the agent, given the state and the episode provided.
+        If less than 500 episodes have passed, a random action is chosen, which
+        promotes early exploration. After 500 episodes, the function has a chance
+        (epsilon) of returning a random action. Otherwise, return the action in the
+        Q-matrix that matches the current state and has the highest expected reward.
+        After 7000 episodes, the chance of returning a random action decreases after
+        each action selection, promoting exploitation.
+
+        :param state: The current state
+        :param episode: The current episode
+        """
+        if episode < 500:
             return np.random.choice(self.action_count)
 
         random_number = np.random.random()
 
-        if episode_index > 7000:
+        if episode > 7000:
             self.epsilon = 0.99999 * self.epsilon
 
         if random_number < self.epsilon:
@@ -108,7 +120,11 @@ class CartPoleQLearningAgent:
                 np.where(self.q_matrix[indexes] == np.max(self.q_matrix[indexes]))[0])
 
     def train(self):
-
+        """
+        Simulates the agent in the environment, for learning. At each time step, chooses and
+        performs an action given the current state and the Q-matrix, and updates the matrix
+        based on the reward it receives after performing the action, using the Q learning algorithm.
+        """
         for episode in range(self.episode_count):
 
             rewards_of_episode = 0
@@ -151,6 +167,9 @@ class CartPoleQLearningAgent:
             self.reward_sum_per_episode.append(rewards_of_episode)
 
     def test(self):
+        """
+        Tests the agent in the simulated environment, given it's current Q-matrix.
+        """
         current_state, _ = self.test_env.reset()
         self.test_env.render()
         time_steps = 1000
